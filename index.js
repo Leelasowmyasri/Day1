8ebb203ba2f6975799a7782d292dbde7ff6c6219
Leelasowmyasri <139006377+Leelasowmyasri@users.noreply.github.com>
2024-07-26 15:13:55 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import Navigation from "./Navigation";
-const rootElement=document.getElementById('root')
-const rootRef=ReactDOM.createRoot(rootElement)
-rootRef.render(<App/>)                                                                                  
